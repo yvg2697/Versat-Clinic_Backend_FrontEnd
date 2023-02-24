77a9c5b27ffa9aa31162cf3495a901abc2cc9748
Gustavo del Sol Hernández <109699202+gdelsol@users.noreply.github.com>
2023-02-24 20:22:00 +0100

    Swagger

--- versat_clinic/versat_clinic/urls.py
+++ versat_clinic/versat_clinic/urls.py
@@ -21,6 +21,9 @@ from django.views.static import serve
 from core.views import front
 from django.views.generic import TemplateView
 from apps.users.views import Login, Logout
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
@@ -30,7 +33,23 @@ from apps.users.views import Login, Logout
 #     # TemplateView.as_view(template_name='frontend/index.html'))
 # ]
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Clínica",
+      default_version='v1',
+      description="Documentación para la API Versat-Clínica",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     # path("", front, name="front"),
     re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}), # only test not production
